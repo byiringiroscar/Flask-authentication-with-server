6547397b4782e0b2335fe803c5f345d4475cde62
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-21 23:27:55 +0200

    wip

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 class ApplicationConfig:
+    SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
