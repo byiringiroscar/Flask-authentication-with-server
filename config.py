6426770e76c756cfb79b33c53cdb620fa63a3cab
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-22 00:02:49 +0200

    Add config of redis

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 
 from dotenv import load_dotenv
+import redis
 import os
 load_dotenv()
 
@@ -8,3 +9,8 @@ class ApplicationConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
+
+    SESSION_TYPE = "redis"
+    SESSION_PERMANENT = False
+    SESSION_USE_SIGNER = True
+    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
