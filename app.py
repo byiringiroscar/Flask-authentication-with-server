6518d3db9175a31967e3b99743c848e704639f6f
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-21 23:45:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask
+from flask import Flask, request, abort, jsonify
 from models import db, User
 from config import ApplicationConfig
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
+bcrypt = Bcrypt(app)
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
 
+@app.route('/register')
+def register_user():
+    email = request.json['email']
+    password = request.json['password']
+
+    user_exists = User.query.filter_by(email=email).first() is not None
+
+    if user_exists:
+        abort(409)
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_user = User(email=email, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({
+        'id': new_user.id,
+        'email': new_user.email
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
