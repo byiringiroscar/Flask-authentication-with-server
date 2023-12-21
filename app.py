d40319a0ed5013b2f27d70fc36fa07cd51930784
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-21 23:56:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,27 @@ def register_user():
         'email': new_user.email
     })
 
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    email = request.json['email']
+    password = request.json['password']
+
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({'error': 'Unauthorized'}), 401
+    
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({'error': 'Unauthorized'}), 401
+    
+    return jsonify({
+        'id': user.id,
+        'email': user.email
+    })
+    
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
