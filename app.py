1dd7758fb36f30639013664232b902555cb1e816
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-22 09:02:07 +0200

    wip

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, session
 from models import db, User
 from config import ApplicationConfig
 from flask_bcrypt import Bcrypt
+from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 bcrypt = Bcrypt(app)
+server_session = Session(app)
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    email = request.json['email']
+    password = request.json['password']
+
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({'error': 'Unauthorized'}), 401
+    
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({'error': 'Unauthorized'}), 401
+    
+    print('---------------', str(user.id))
+    
+    session["user_id"] = str(user.id)
+    
+    return jsonify({
+        'id': user.id,
+        'email': user.email
+    })
+    
+
+
 
 @app.route('/register', methods=['POST'])
 def register_user():
@@ -29,25 +57,6 @@ def register_user():
         'email': new_user.email
     })
 
-
-@app.route('/login', methods=['POST'])
-def login_user():
-    email = request.json['email']
-    password = request.json['password']
-
-    user = User.query.filter_by(email=email).first()
-
-    if user is None:
-        return jsonify({'error': 'Unauthorized'}), 401
-    
-    if not bcrypt.check_password_hash(user.password, password):
-        return jsonify({'error': 'Unauthorized'}), 401
-    
-    return jsonify({
-        'id': user.id,
-        'email': user.email
-    })
-    
 
 
 
